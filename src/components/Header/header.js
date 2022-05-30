fea6866b9d40debc22837812c3a1629ea5e78ce8
Ziomara Jiménez <97549552+ziomarajimenez@users.noreply.github.com>
2022-05-30 09:33:39 -0600

    Changes notifications icon for a purchase icon, and when it's selected the user gets  redirected to /activeorders

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,19 +1,20 @@
 import logo from '../../assets/BQ logo sin fondo.png'
-/* import notificationIcon from '../../assets/Notification.png'; */
+import purchaseIcon from '../../assets/purchaseicon.png';
 import profileIcon from '../../assets/Profile.png';
 import ProfileModal from '../ProfileModal/ProfileModal';
 import { useState } from 'react';
 import { currentUser, logOut } from '../../lib/firebaseAuth';
+import { useNavigate } from "react-router-dom";
 
 export const Header = () => {
+    const navigate = useNavigate();
     const [isOpen, setIsOpen] = useState(false);
 
     return (
         <header className='header'>
             <img src={logo} alt="burger queen logo" className='bq-logo-header'></img>
-            {/* <img src={notificationIcon} alt="notification icon" className='icon'></img> */}
+            {<img src={purchaseIcon} alt="purchase icon" className='icon' id="purchaseIcon" onClick={() => navigate('/activeorders')}></img>}
             <img src={profileIcon} alt="profile icon" className='icon' onClick={() => setIsOpen(true)}></img>
-
             <ProfileModal open={isOpen} onClose={() => setIsOpen(false)} user={currentUser()} logOut={logOut} />
         </header>
     );
