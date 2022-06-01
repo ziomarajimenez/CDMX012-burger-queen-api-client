c3a19bca5dcb9d368e49af9ba3da8d98b08215fc
maisahr <ma.isahr7@gmail.com>
2022-06-01 11:02:14 -0700

    Merge remote-tracking branch 'origin/main' into develop3

--- src/components/EmployeesList/EmployeesList.js
+++ src/components/EmployeesList/EmployeesList.js
@@ -1,21 +1,6 @@
 import './EmployeesList.css';
 import { ModifyEmployee } from "../Modify Employee/ModifyEmployee";
-import { useEffect, useState } from 'react';
 export const EmployeesList = (props) => {
-    const [employees, setEmployees] = useState();
-
-    useEffect(() => {
-        fetch('http://localhost:3333/users')
-            .then((response) => {
-                return response.json()
-            })
-            .then((employees) => {
-                setEmployees(employees);
-                console.log(employees)
-            })
-    }, [])
-
-    //console.log(employees)
 
     return (
         <div className="table-wrapper">
